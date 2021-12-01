9b9fa4a741a4c82a757f1b1ea8f68ec68617c149
‘pooria’ <pooriasharifi577@gmail.com>
2021-12-01 19:18:21 +0330

    final

--- lib/module/server.dart
+++ lib/module/server.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // var uri = 'http://127.0.0.1:5000';
-var uri = 'http://192.168.1.4:3000';
+var uri = 'http://194.59.170.176:5000';
 
 class user_info {
   int _id;
